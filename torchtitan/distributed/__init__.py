c3447c7b5fc48a8ad1192672391ceb8b7d032c10
Neil Getty <ngetty@aurora-uan-0010.hostmgmt.cm.aurora.alcf.anl.gov>
2026-02-13 18:57:39 +0000

    Relocate XPUExpertParallel into the distributed package

--- torchtitan/distributed/__init__.py
+++ torchtitan/distributed/__init__.py
@@ -13,10 +13,16 @@ from torch.distributed.tensor.parallel import ParallelStyle
 from torch.distributed.tensor.placement_types import Placement
 
 from torchtitan.distributed.parallel_dims import ParallelDims
+from torchtitan.distributed.expert_parallel_xpu import (
+    get_expert_parallel_class,
+    XPUExpertParallel,
+)
 
 __all__ = [
     "ParallelDims",
     "NoParallel",
+    "XPUExpertParallel",
+    "get_expert_parallel_class",
 ]
 
 
